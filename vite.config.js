6588c249fdedc27506b228a8d6a821a72cce8e54
Shams Gadirli <shams.kadirli@gmail.com>
2023-08-10 18:29:39 +0400

    Deployment setup

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/rolling-dice-game",
-  plugins: [react()],
+  plugins: [react()], 
+  base: "/rolling-dice-game/"
 })
